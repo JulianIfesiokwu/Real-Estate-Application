559dded1a9ce5c5128e64e97a52d69144fe1dc03
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2023-12-30 14:08:36 +0100

    Feat: connecting to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to Mongoose");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(3000, (err, res) => {
   console.log("Server is running on Port 3000");
