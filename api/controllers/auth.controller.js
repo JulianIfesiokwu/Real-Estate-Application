61f8a149b9de089eeff333d5440467ceeca82d87
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2023-12-31 16:17:39 +0100

    Feat: creating signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +12,35 @@ export const signUp = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email: email });
+    // CHECK FOR USER IN DATABASE
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    // COMPARE USER PASSWORD WITH STORED PASSWORD
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Please check your credentials"));
+    }
+    // CREATE JWT TOKEN FOR USER
+    const token = jwt.sign({ id: validPassword._id }, process.env.JWT_SECRET);
+
+    // RETURN DATA WITH PASSWORD
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
