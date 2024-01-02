0f320e7f52a372d9752cb79656b75b32ec04ee2c
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2024-01-02 13:24:35 +0100

    Feat: creating and adding OAuth component

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHspKGSu0IJAQ4Me2JTfp2V4e2Ttt5v4Y",
+  authDomain: "real-estate-project-b9093.firebaseapp.com",
+  projectId: "real-estate-project-b9093",
+  storageBucket: "real-estate-project-b9093.appspot.com",
+  messagingSenderId: "205521384793",
+  appId: "1:205521384793:web:f8aec67e80a76a25f5c848",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
