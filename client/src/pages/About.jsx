f49cda210b4e042c6dbcaf4f19aad9a30df3d5fe
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2024-01-16 16:31:35 +0100

    Feat: complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,27 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Sahand estate
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit autem
+        delectus, unde distinctio amet consequuntur, animi doloremque,
+        aspernatur eligendi deleniti obcaecati nulla. Sunt natus quisquam
+        mollitia quod enim deleniti nobis!
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit autem
+        delectus, unde distinctio amet consequuntur, animi doloremque,
+        aspernatur eligendi deleniti obcaecati nulla. Sunt natus quisquam
+        mollitia quod enim deleniti nobis!
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit autem
+        delectus, unde distinctio amet consequuntur, animi doloremque,
+        aspernatur eligendi deleniti obcaecati nulla. Sunt natus quisquam
+        mollitia quod enim deleniti nobis!
+      </p>
+    </div>
+  );
 }
